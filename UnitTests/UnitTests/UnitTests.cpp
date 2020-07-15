3740d29f1ba082bec61e543bc3a051ebc17a3a99
ivpopova <ivanapopova99@abv.bg>
2020-07-15 12:04:38 +0300

    add BankTests

--- UnitTests/UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests/UnitTests.cpp
@@ -56,6 +56,105 @@ namespace UnitTests
 		}
 	};
 
-	
+	TEST_CLASS(BankTests)
+	{
+	public:
+		std::map<std::string, Customer*> customers;
+		std::map<std::string, Account*> accounts;
+		std::vector<Employee*> employees;
+
+		Manager* manager = new Manager("Ivan Ivanov", 55, employees, 20000);
+		Bank* bank = new Bank("DEMO BANK SYSTEM", manager, customers, accounts);
+
+		const std::string NAME = "Peter Petrov";
+		const std::string IBAN = "11BBN99";
+		const std::string CUSTOMER_ID = "7766";
+		const double INTEREST_RATE = 0.7;
+		const std::string BUSINESS_TYPE = "Large";
+
+		const int AGE = 22;
+		const double ACCOUNT_BALANCE = 1000;
+		const double AMOUNT = 200;
+		TEST_METHOD(BankDepositTest)
+		{
+			Customer* customer = new Customer(NAME, AGE, CUSTOMER_ID);
+			bank->addCheckingAccount(IBAN, CUSTOMER_ID, ACCOUNT_BALANCE);
+
+			bank->deposit(IBAN, AMOUNT);
+
+			Assert::AreEqual(ACCOUNT_BALANCE + AMOUNT, bank->getCurrentAccount(IBAN)->getBalance());
+		}
+
+		TEST_METHOD(BankWithdrawTest)
+		{
+			Customer* customer = new Customer(NAME, AGE, CUSTOMER_ID);
+			bank->addCheckingAccount(IBAN, CUSTOMER_ID, ACCOUNT_BALANCE);
+
+			bank->withdraw(IBAN, AMOUNT);
+
+			Assert::AreEqual(ACCOUNT_BALANCE - AMOUNT, bank->getCurrentAccount(IBAN)->getBalance());
+		}
+
+		TEST_METHOD(BankTransferTest)
+		{
+			std::string senderIBAN = IBAN;
+			std::string senderID = CUSTOMER_ID;
+
+			std::string receiverIBAN = "12345890NN";
+			std::string receiverID = "123456";
+			double receiverBalance = 1200;
+
+			bank->addCheckingAccount(IBAN, CUSTOMER_ID, ACCOUNT_BALANCE);
+			bank->addCheckingAccount(receiverIBAN, receiverID, receiverBalance);
+
+			bank->transfer(senderIBAN, receiverIBAN, AMOUNT);
+
+			Assert::AreEqual(ACCOUNT_BALANCE - AMOUNT, bank->getCurrentAccount(IBAN)->getBalance());
+		}
+
+		TEST_METHOD(BankAddCustomerTest)
+		{
+			bank->addCustomer(NAME, AGE, CUSTOMER_ID);
+
+			Assert::IsTrue(bank->isCustomerExist(CUSTOMER_ID));
+		}
+
+		TEST_METHOD(BankAddBusinessCustomerTest)
+		{
+			bank->addBusinessCustomer(NAME, AGE, CUSTOMER_ID, BUSINESS_TYPE);
+
+			Assert::IsTrue(bank->isCustomerExist(CUSTOMER_ID));
+		}
+		TEST_METHOD(BankAddCheckingAccountTest)
+		{
+			bank->addCheckingAccount(IBAN, CUSTOMER_ID, ACCOUNT_BALANCE);
+
+			Assert::IsTrue(bank->isIbanExist(IBAN));
+		}
+
+		TEST_METHOD(BankAddSavingsAccountTest)
+		{
+			bank->addSavingsAccount(IBAN, CUSTOMER_ID, ACCOUNT_BALANCE, INTEREST_RATE);
+
+			Assert::IsTrue(bank->isIbanExist(IBAN));
+		}
+
+		TEST_METHOD(BankDeleteCustomerTest)
+		{
+			bank->addCustomer(NAME, AGE, CUSTOMER_ID);
+
+			bank->deleteCustomer(CUSTOMER_ID);
+
+			Assert::IsFalse(bank->isCustomerExist(CUSTOMER_ID));
+		}
+
+		TEST_METHOD(BankDeleteAccountTest)
+		{
+			bank->addCheckingAccount(IBAN, CUSTOMER_ID, ACCOUNT_BALANCE);
+
+			bank->deleteAccount(IBAN);
+
+			Assert::IsFalse(bank->isIbanExist(IBAN));
+		}
 	};
 }
